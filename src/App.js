982aca82cdd9997b375734d1c484d4eb806b05ed
aj068102 <aj068102@cerner.net>
2021-05-27 18:44:27 +0530

    Created requests temps, Row component and Axios create method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Building Netflix Clone</h1>
+      <Row title= "Netflix Originals"/>
+      <Row title= "Trending Now"/>
     </div>
   );
 }
